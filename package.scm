31e24087581b13b7183023c01f231c4421d0821f
Leah Neukirchen <leah@vuxu.org>
2024-05-09 18:54:36 +0200

    license under same terms as BLAKE3

--- package.scm
+++ package.scm
@@ -2,6 +2,6 @@
   :version "0.1"
   :description "The BLAKE3 cryptographic hash function"
   :authors ("Leah Neukirchen <leah@vuxu.org>")
-  :licenses ("CC0")
+  :licenses ("CC0, Apache-2.0")
   :repository "https://github.com/leahneukirchen/blake3"
   :providing-modules (blake3))
